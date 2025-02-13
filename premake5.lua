657481a7f13e628d7d68b6ef2ae981a1f8e84ff7
FastIgnitionStudios <jlb@fastignitionstudios.com>
2025-02-13 20:46:32 +0000

    premake

---
+++ premake5.lua
@@ -0,0 +1,55 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("%{wks.location}/Build/Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/Build/Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "imstb_truetype.h",
+        "imstb_textedit.h",
+        "imstb_rectpack.h",
+        "imgui_widgets.cpp",
+        "imgui_tables.cpp",
+        "imgui_internal.h",
+        "imgui_draw.cpp",
+        "imgui_demo.cpp",
+        "imgui.h",
+        "imgui.cpp",
+        "imconfig.h",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_vulkan.h",
+        "backends/imgui_impl_vulkan.cpp"
+	}
+
+    includedirs
+    {
+        "",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.VulkanSDK}"
+        }
+
+    links
+    {
+        "%{Library.Vulkan}"
+        }
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On" --staticly linking the runtime libraries
+
+		defines
+		{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
